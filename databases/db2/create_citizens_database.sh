42c6bb3daa7cc715e2aeff02a0399b3823ffbbc1
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-02-11 12:04:17 +0200

    added db2 sample db

---
+++ databases/db2/create_citizens_database.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+source ${SETUPDIR?}/include/db2_constants
+source ${SETUPDIR?}/include/db2_common_functions
+
+#functions
+#create schema
+
+create_demo_schema_and_tables()
+{
+    echo "(*) Creating demo schema and tables..."
+    su - ${DB2INSTANCE?} -c "db2 connect to ${DBNAME?} && \
+    db2 -tvmf /tmp/create_schema_and_tables.sql |tee /tmp/create_schema_and_tables.sql.out && \
+    db2 connect reset"
+    echo "(*) Creating demo schema and tables...done."
+}
+
+insert_citizens()
+{
+    echo "(*) Inserting citizens..."
+    su - ${DB2INSTANCE?} -c "db2 connect to ${DBNAME?} && \
+    db2 load from /tmp/citizens.csv of del replace into demo.citizens && \
+    db2 connect reset"
+    #db2-tvmf for debugging
+    #db2 -tf /tmp/insert_citizens.sql > /tmp/insert_citizens.sql.out && \
+    echo "(*) Inserting citizens...done."
+}
+
+create_demo_schema_and_tables
+insert_citizens
---
+++ databases/db2/create_schema_and_tables.sql
@@ -0,0 +1,5 @@
+create schema demo;
+
+create table demo.citizens (id char(12) UNIQUE NOT null,firstname varchar(16),lastname varchar(64),birthdate date); 
+
+
