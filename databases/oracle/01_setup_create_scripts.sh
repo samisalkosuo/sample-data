947d88d6a04624aea72d316fd94ba13db27bc97e
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-02-12 11:09:42 +0200

    added Oracle demo database

---
+++ databases/oracle/01_setup_create_scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat << EOF > /opt/oracle/scripts/startup/02_create_user.sql
+alter session set "_ORACLE_SCRIPT"=true;
+create user ${ORACLE_USER} identified by ${ORACLE_PWD};
+grant dba to ${ORACLE_USER};
+EOF
+
+cat << EOF > /opt/oracle/scripts/startup/03_create-table.sql
+create table ${ORACLE_USER}.phonenumbers (id char(17) unique not null, phonenumber char(10));
+EOF
+
+cat << EOF > /opt/oracle/scripts/startup/04-insert-data.sh
+#!/bin/sh
+sqlldr userid=${ORACLE_USER}/${ORACLE_PWD} data=/tmp/phonenumbers.csv control=/opt/oracle/scripts/startup/load-phonenumbers.ctl
+EOF
+
---
+++ databases/oracle/load-phonenumbers.ctl
@@ -0,0 +1,8 @@
+options(direct=true)
+load data
+into table phonenumbers
+fields terminated by ',' optionally enclosed by '"' TRAILING NULLCOLS
+( 
+    id,
+    phonenumber
+)
