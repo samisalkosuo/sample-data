d79fbe1b64e92a4be3baece9bead9e17f3cc7211
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-02-13 10:48:23 +0200

    updated

--- databases/db2/create_schema_and_tables.sql
+++ databases/db2/create_schema_and_tables.sql
@@ -1,5 +1,5 @@
 create schema demo;
 
-create table demo.citizens (id char(12) UNIQUE NOT null,firstname varchar(16),lastname varchar(64),birthdate date); 
+create table demo.citizens (id varchar(16) UNIQUE NOT null,firstname varchar(20),lastname varchar(64),birthdate date); 
 
 
