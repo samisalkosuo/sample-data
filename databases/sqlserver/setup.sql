1631c1a27d7fd5044b2a295d466646fab82893e5
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-02-27 11:09:07 +0200

    updated data

--- databases/sqlserver/setup.sql
+++ databases/sqlserver/setup.sql
@@ -2,6 +2,6 @@ CREATE DATABASE demo;
 GO
 USE demo;
 GO
-CREATE TABLE Addresses (ID char(17) unique not null, address nvarchar(64), postalNumber char(10),municipality nvarchar(64));
-CREATE TABLE Professions (ID char(17) unique not null, profession nvarchar(96));
+CREATE TABLE addresses (id int primary key not null, address nvarchar(64), postalnumber char(10),municipality nvarchar(64));
+CREATE TABLE professions (id int primary key not null, profession nvarchar(96));
 GO
