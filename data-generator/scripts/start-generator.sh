cba3e50de5c2cc1547c1e02203b9661d9755619d
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-02-28 11:13:37 +0200

    updated

--- data-generator/scripts/start-generator.sh
+++ data-generator/scripts/start-generator.sh
@@ -18,8 +18,8 @@ function createInsertCDRSQL
     #echo $SQL_LINES
     local SQL=$(cat insert_cdr.sql | awk '{print}' ORS=' ')
     #insert to Postgres
-    echo | awk -v SQL="$SQL" -v CDR_DB_JDBC_URL="$CDR_DB_JDBC_URL" -v CDR_DB_USER_NAME="$CDR_DB_USER_NAME" -v CDR_DB_USER_PASSWORD="$CDR_DB_USER_PASSWORD" '{print "java -jar /datagenerator/jsql.jar -j " CDR_DB_JDBC_URL " -u " CDR_DB_USER_NAME " -p " CDR_DB_USER_PASSWORD " -s \"" SQL "\" > /dev/null"}' | sh
-    log "Inserted ${SQL_LINES} call detail records to CDR database."
+    log "Inserting ${SQL_LINES} call detail records to CDR database..."
+    echo | awk -v SQL="$SQL" -v CDR_DB_JDBC_URL="$CDR_DB_JDBC_URL" -v CDR_DB_USER_NAME="$CDR_DB_USER_NAME" -v CDR_DB_USER_PASSWORD="$CDR_DB_USER_PASSWORD" '{print "java -jar /datagenerator/jsql.jar -j " CDR_DB_JDBC_URL " -u " CDR_DB_USER_NAME " -p " CDR_DB_USER_PASSWORD " -s \"" SQL "\""}' | sh
     
     cd ..
 }
